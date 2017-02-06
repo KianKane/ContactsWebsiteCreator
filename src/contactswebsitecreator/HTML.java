9483bd4ffb484489536168eac77d3cadeb1136fc
Kian Kane <AmethystArchon@gmail.com>
2017-02-06 09:28:17 +0000

    Created HTML class which creates html strings

---
+++ src/contactswebsitecreator/HTML.java
@@ -0,0 +1,55 @@
+package contactswebsitecreator;
+
+/**
+ * Used to create HTML strings
+ * 
+ * @author Kian Kane
+ * @version 06/02/2017
+ */
+public class HTML
+{
+    public static String doctype()
+    {
+        return "<!DOCTYPE html>";
+    }
+    
+    public static String html(String innerHTML)
+    {
+        return "<html>" + innerHTML + "</html>";
+    }
+    
+    public static String head(String innerHTML)
+    {
+        return "<head>" + innerHTML + "</head>";
+    }
+    
+    public static String title(String innerHTML)
+    {
+        return "<title>" + innerHTML + "</title>";
+    }
+    
+    public static String body(String innerHTML)
+    {
+        return "<body>" + innerHTML + "</body>";
+    }
+    
+    public static String h1(String innerHTML)
+    {
+        return "<h1>" + innerHTML + "/h1>";
+    }
+    
+    public static String p(String innerHTML)
+    {
+        return "<p>" + innerHTML + "</p>";
+    }
+   
+    public static String br()
+    {
+        return "<br>";
+    }
+    
+    public static String a(String href, String innerHTML)
+    {
+        return "<a href=\"" + href + "\">" + innerHTML + "</a>";
+    }
+}
